8d17a4c33997ebd75dbece39420537104c6b4fb9
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2023-02-21 23:50:44 +0300

    fix mistake

--- script/index.js
+++ script/index.js
@@ -1,27 +1,26 @@
-const popup=document.querySelector(".popup");
-const openButton=document.querySelector(".profile__editButton_act");
-const popupExit=document.querySelector(".popup__exit");
-const getInput=document.querySelector(".profile__name");
-const getJob=document.querySelector(".profile__job");
-const nameInput=document.querySelector(".popup__input-name");
-const jobInput=document.querySelector(".popup__input-job");
-const popupSaved=document.querySelector(".popup__saved");
-const form=document.querySelector(".popup__form")
-console.log(popupSaved)
-function openPopup () {
-    popup.classList.add ('popup__opened');
-    nameInput.value= getInput.textContent;
-    jobInput.value=getJob.textContent;
-  }
-  
-function closePopup ()  {
-    popup.classList.remove ('popup__opened');}
-
-function handleFormSubmit () {
-    getInput.textContent = nameInput.value;
-    getJob.textContent = jobInput.value;
-    closePopup();
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+const openButton = document.querySelector('.profile__editbutton');
+const popupExit = document.querySelector('.popup__exit');
+const getInput = document.querySelector('.profile__name');
+const getJob = document.querySelector('.profile__job');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = getInput.textContent;
+  jobInput.value = getJob.textContent;
 }
-openButton.addEventListener("click",openPopup);
-popupExit.addEventListener("click",closePopup);
-popupSaved.addEventListener("click", handleFormSubmit); 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function Submit(evt) {
+  evt.preventDefault();
+  getInput.textContent = nameInput.value;
+  getJob.textContent = jobInput.value;
+  closePopup();
+}
+openButton.addEventListener('click', openPopup);
+popupExit.addEventListener('click', closePopup);
+popupContainer.addEventListener('submit', Submit);
