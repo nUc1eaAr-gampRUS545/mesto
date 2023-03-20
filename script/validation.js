3674a3e3428a3cf0756d19d4034b9b49029148ed
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2023-03-20 15:59:47 +0300

    fasefse

--- script/validation.js
+++ script/validation.js
@@ -68,7 +68,7 @@ enableValidation({
     popupAddCardSelector:".popup_type_add-cards",
     popupProfileSelector:".popup_type_profile",
     inputSelector:".popup__input",
-    errorStr:".popup__message_type_",
+    errorStr:".popup__message__",
     submitButonSelector:".popup__saved",
     popupInputErrorClass:"popup__input-error",
     invalidSubmitButtonClass:"popup__saved-invalid",
